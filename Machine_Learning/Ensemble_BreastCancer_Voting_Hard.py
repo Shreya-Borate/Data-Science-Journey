bebb6697e9a48f364b1ec2d800975c1503ce535d
Shreya-Borate <borateshreya43@gmail.com>
2026-09-12 22:00:36 +0530

    Ensemble Learning - Hard Voting Algorithm

---
+++ Machine_Learning/Ensemble_BreastCancer_Voting_Hard.py
@@ -0,0 +1,99 @@
+import pandas as pd
+
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import StandardScaler
+from sklearn.metrics import accuracy_score, confusion_matrix
+
+from sklearn.linear_model import LogisticRegression
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.neighbors import KNeighborsClassifier
+
+from sklearn.ensemble import VotingClassifier
+
+#----------------------------------
+# Step 1 : Load the Dataset
+#----------------------------------
+
+df = pd.read_csv("breast_cancer.csv")
+
+print("Shape of Dataset : ",df.shape)
+
+print("First few records : ")
+print(df.head())
+
+#--------------------------------------------------
+# Step 2 : Separate Features and labels
+#--------------------------------------------------
+
+X = df.drop("target", axis=1)
+Y = df["target"]
+
+print("X shape : ",X.shape)
+print("Y shape : ",Y.shape)
+
+
+#--------------------------------------------------
+# Step 3 : Split dataset for traning and testing
+#--------------------------------------------------
+
+X_train, X_test, Y_train, Y_test = train_test_split(X, Y, test_size=0.2, random_state=42)
+
+
+#--------------------------------------------------
+# Step 4 : Scale the Features
+#--------------------------------------------------
+
+scaler =  StandardScaler()
+
+X_train = scaler.fit_transform(X_train)
+X_test = scaler.fit_transform(X_test)
+
+
+#--------------------------------------------------
+# Step 5.1 : Create the Individual Models
+#--------------------------------------------------
+
+model_log = LogisticRegression(max_iter=1000)
+model_det = DecisionTreeClassifier(random_state=42)
+model_KNN = KNeighborsClassifier(n_neighbors=5)
+
+
+#--------------------------------------------------
+# Step 5.2 : Create the Voting Models
+#--------------------------------------------------
+
+model = VotingClassifier(
+    estimators=[
+        ('logistic',model_log),
+        ('decision_tree',model_det),
+        ('knn',model_KNN)
+    ],
+    voting='hard'
+)
+
+#--------------------------------------------------
+# Step 6 : Train The Model
+#--------------------------------------------------
+
+model = model.fit(X_train, Y_train)
+
+
+#--------------------------------------------------
+# Step 7 : Test the Model
+#--------------------------------------------------
+
+y_pred = model.predict(X_test)
+
+
+#--------------------------------------------------
+# Step 8 : Evaluate the Model
+#--------------------------------------------------
+
+print("Accuracy : ",accuracy_score(Y_test,y_pred))
+
+print("Confusion matrix : ")
+print(confusion_matrix(Y_test,y_pred))
+
+
+
+
